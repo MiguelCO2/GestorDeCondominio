de4c49df98dc335a699cbe83439392dae746bcf0
MiguelCO2 <miguelcharmelo13@gmail.com>
2026-04-25 02:10:32 -0400

    Login con vista provisional

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -20,4 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('apps.announcements.urls')),
+    
+    path('api/auth/', include('apps.accounts.urls')),
 ]
